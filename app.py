d346a88fc4a1bd5ce9b4a3844489dd3cfaeee01e
suprajashetty <suprajashetty84@gmail.com>
2026-03-06 18:28:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,18 +33,12 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 # =========================
 # MongoDB Setup
 # =========================
-MONGO_URI = "mongodb://localhost:27017/"
-mongo_client = MongoClient(MONGO_URI)
-db = mongo_client["stego_app"]
-logs_col = db["logs"]
-
+# =========================
+# Logging (temporary for Render)
+# =========================
 def log_action(action_type, filename=None, details=None):
-    logs_col.insert_one({
-        "timestamp": datetime.datetime.utcnow(),
-        "action": action_type,
-        "file": filename,
-        "details": details
-    })
+    # Print log instead of saving to MongoDB
+    print(f"[LOG] {datetime.datetime.utcnow()} | Action: {action_type} | File: {filename} | Details: {details}")
 
 # =========================
 # Device
